61c4ca71f58fd5aa659ab568dcf6430352ee54d7
haroonnisar111 <66691075+haroonnisar111@users.noreply.github.com>
2025-06-27 21:41:21 +0530

    finally fixed

--- src/main/java/com/budgetbuddy/personal_finance_tracker/PersonalFinanceTrackerApplication.java
+++ src/main/java/com/budgetbuddy/personal_finance_tracker/PersonalFinanceTrackerApplication.java
@@ -2,16 +2,10 @@ package com.budgetbuddy.personal_finance_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.budgetbuddy.personal_finance_tracker.entity")
-@EnableJpaRepositories("com.budgetbuddy.personal_finance_tracker.repository")
 public class PersonalFinanceTrackerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalFinanceTrackerApplication.class, args);
 	}
-
 }
